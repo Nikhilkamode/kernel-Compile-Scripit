07e9337e6aa989a3c2b10f88e48470e14cace376
Nikhil Kamode <112844869+Nikhilkamode@users.noreply.github.com>
2024-06-12 23:26:33 +0530

    clone.sh

---
+++ clone.sh
@@ -0,0 +1,9 @@
+# Kernel Clonning Script
+
+git clone https://github.com/Nikhilkamode/kernel-Compile-Scripit
+git clone --depth=1 https://github.com/NikhilKamode/AnyKernel3 Anykernel
+git clone --depth=1 https://github.com/kdrag0n/proton-clang CLANG-13
+
+# Start Build
+
+bash run.sh
